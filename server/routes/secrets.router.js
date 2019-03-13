38859ab8e14216491cb40b565b4faf6de25dc349
Todd Boyce Jr <tjboyce@me.com>
2019-03-13 16:20:14 -0500

    base complete

---
+++ server/routes/secrets.router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('req.user:', req.user);
+    const queryText = 'SELECT * FROM "secret" WHERE "secrecy_level" <=$1';
+    pool.query(queryText, [req.user.clearance_level])
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+});
+
+module.exports = router;
